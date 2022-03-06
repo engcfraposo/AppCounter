fb03a67795565999d74069e9da12bb2ff481e33a
Cláudio Rapôso <engcfraposo@gmail.com>
2022-03-06 08:29:47 -0300

    Initial Commit

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by claudio.f.raposo on 06/03/22.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
